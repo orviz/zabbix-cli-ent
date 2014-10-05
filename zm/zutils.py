12a8d8bc1f6ffa148a3ade329b08eb09c867878e
Pablo Orviz <orviz@ifca.unican.es>
2014-10-05 11:00:23 +0200

    Add template-list functionality.

--- zm/zutils.py
+++ zm/zutils.py
@@ -1,9 +1,13 @@
 
-def get(conn, type, parameter, value, output="extend"):
+def get(conn, type, parameter, value, output="extend", **kwargs):
     types = {
         "template": conn.template.get,
         "host": conn.host.get,
+        "hostgroup": conn.hostgroup.get,
     }
-    return types[type](filter={parameter: value}, output=output)
+    if parameter:
+        return types[type](filter={parameter: value}, output=output, **kwargs)
+    else:
+        return types[type](output=output, **kwargs)
 
 
