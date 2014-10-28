0f4c769157b84aab65f9571c66e68701478cf5c5
Pablo Orviz <orviz@ifca.unican.es>
2014-10-28 09:47:16 +0100

    item-[enable|disable] commands accept item as a name description.

--- zm/item.py
+++ zm/item.py
@@ -22,19 +22,57 @@ def list(conn, host):
 
     l = []
     for item in zm.zutils.get(conn,
-                        type="item",
-                        parameter=parameter,
-                        value=host,
-                        output=["status", "name"]):
+                              type="item",
+                              parameter=parameter,
+                              value=host,
+                              output=["status", "name"]):
         l.append([item["itemid"], item["name"], ITEM_STATUS[item["status"]]])
     if l:
         zm.utils.print_table(["ID", "Name", "Value"], sorted(l))
 
+
 @zm.utils.login
-def update(conn, id, status):
+def update(conn, id, status, host=None, hostgroup=None):
+    items = []
+    for narg in id:
+        try:
+            int(narg)
+            parameter = "itemid"
+        except ValueError:
+            parameter = "name"
+
+        items.extend(zm.zutils.get(conn,
+                                   type="item",
+                                   parameter=parameter,
+                                   value=narg,
+                                   output=["status", "name"],
+                                   **{"selectHosts": ["name"]}))
+
+    if host or hostgroup:
+        hitems = zm.zutils.get(conn,
+                              type="item",
+                              output=["status", "name"],
+                              **{"filter": {"name": id,
+                                            "host": host,
+                                            "hostgroup": hostgroup},
+                                 "selectHosts": ["name"]})
+
+        l_items = [i["itemid"] for i in items]
+        for hi in hitems:
+            if not hi["itemid"] in l_items:
+                items.append(hi)
+
     try:
-        conn.item.update(itemid=id, status=str(status))
-        print("Item %s %s" % (id, ITEM_STATUS[str(status)].lower()))
+        for item in items:
+            conn.item.update(itemid=item["itemid"], status=str(status))
+            print("Item <%s (%s)> %s on host %s" % (item["name"],
+                                                    item["itemid"],
+                                                    ITEM_STATUS[str(status)].lower(),
+                                                    item["hosts"][0]["name"]))
+        if not items:
+            print("No matching item found")
     except pyzabbix.ZabbixAPIException, e:
         if e.args[1] == -32602:
             print("Permission denied: failed to enable item.", file=sys.stderr)
+
+
